bbd6fe04d70cb6a9c84d373654856587cb57fa4b
Ivan Smollet <smollet777@bigmir.net>
2019-04-06 18:54:03 +0300

    feat: infinite scroll

--- src/app/core/services/search.service.ts
+++ src/app/core/services/search.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { Observable, Subject } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +9,13 @@ export class SearchService {
 
   private _query: string;
   private readonly querySubject = new Subject<string>();
+  index$ = new BehaviorSubject(0);
+  private _chunkSize = 25;
 
   set query(query: string) {
     this._query = query;
     this.querySubject.next(query);
+    this.index$.next(0);
   }
 
   get query(): string {
@@ -21,5 +24,32 @@ export class SearchService {
 
   get query$(): Observable<string> {
     return this.querySubject.asObservable();
+  }
+
+  set chunkSize(value: number) {
+    if (value > 0) {
+      this._chunkSize = value;
+    }
+  }
+
+  get chunkSize(): number {
+    return this._chunkSize;
+  }
+
+  nextChunk(): number {
+    const value = this.index$.value + this._chunkSize;
+    this.index$.next(value);
+
+    return value;
+  }
+
+  prevChunk(): boolean | number {
+    const value = this.index$.value - this._chunkSize;
+    if (value < 0) {
+      return false;
+    }
+    this.index$.next(value);
+
+    return value;
   }
 }
